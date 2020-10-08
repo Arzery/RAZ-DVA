3674698d3deea4f0c14304c8721f9aab86673fdb
Arzery <63201065+Arzery@users.noreply.github.com>
2020-10-08 21:23:04 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 	$('.btn').click(function(event) {
+		$('.bmv').css('display','none');
+	});
+
+	$('.bmv-btn').click(function(event) {
+		$('.bmv').css('display','block');
+	});
+
+	$('.btn').click(function(event) {
 		$('.block').removeClass('active')
 		var num = $(this).attr('data-num');
 		$('#block'+num).addClass('active')
 	});
+	
+
+	
 });
